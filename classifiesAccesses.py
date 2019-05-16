71f70620c7aaf064d3152c1744e581b6b9896793
Ramon Ferreira <ramonfsk@gmail.com>
2019-05-16 16:30:21 -0300

    ml v2

--- classifiesAccesses.py
+++ classifiesAccesses.py
@@ -1,18 +1,25 @@
 from dados import loadAccess
 X,Y = loadAccess()
+# a abordagem utilizada foi separar os dados para treino e teste em proporação 90/10
+#a taxa foi de 88.89%
+trainingData = X[:90]
+trainingMarkings = Y[:90]
+
+testData = X[-9:]
+testMarkings = Y[-9:]
 
 from sklearn.naive_bayes import MultinomialNB
 model = MultinomialNB()
-model.fit(X, Y)
+model.fit(trainingData, trainingMarkings)
 
 print(model.predict([[1,0,1], [0,1,0], [1,0,0], [1,1,0], [1,1,1]]))
 
-result = model.predict(X)
-difference = result - Y
+result = model.predict(testData)
+difference = result - testMarkings
 hits = [d for d in difference if d == 0]
 totalHits = len(hits)
-totalElements = len(X)
-taxHit = 100.00 * totalHits / totalElements
+totalElements = len(testData)
+taxHit = 100.0 * totalHits / totalElements
 
 print(taxHit)
 print(totalElements)
